47857900b2d61f3a4f1f41ca782c5c0781673ca7
Erick <erickluques@gmail.com>
2019-09-01 09:17:14 -0300

    Método construct AknaController

--- app/Http/Controllers/Emkt/AknaController.php
+++ app/Http/Controllers/Emkt/AknaController.php
@@ -10,4 +10,17 @@ class AknaController extends Controller
 {
     public $endPoint = 'http://app.akna.com.br/emkt/int/integracao.php';
 
+    public $storagePath = '';
+
+    public $data;
+
+    public $codigosIes;
+
+    public function __construct()
+    {
+        $this->storagePath = storage_path().'/akna_xml';
+        $this->data = include __DIR__.'/config/user.php';
+        $this->codigosIes = include __DIR__.'/config/client-codes.php';
+    }
+
 }

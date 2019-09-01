de9641249751146f2fb454cc747044c27eb20a9a
Erick <erickluques@gmail.com>
2019-09-01 07:27:20 -0300

    Update PlanilhaController.php

--- app/Http/Controllers/PlanilhaController.php
+++ app/Http/Controllers/PlanilhaController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Maatwebsite\Excel\Facades\Excel;
 use App\Http\Controllers\Controller;
+use Chumper\Zipper\Zipper;
+use illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Facades\DB;
 use App\Planilhas\Planilha;
 use Session;
 

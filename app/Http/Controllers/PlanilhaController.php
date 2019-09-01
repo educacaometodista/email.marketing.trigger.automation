241fb7346cc2b71ebe84f6afbf626fb3b61dd17d
Erick <erickluques@gmail.com>
2019-09-01 07:23:09 -0300

    Create PlanilhaController

---
+++ app/Http/Controllers/PlanilhaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Planilhas\Planilha;
+use Session;
+
+
+class PlanilhaController extends Controller
+{
+    //
+}

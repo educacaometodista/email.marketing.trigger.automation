47b67f0e57990fcc837852aec255909685835ac5
erickfirmo <erickluques@gmail.com>
2019-09-03 14:23:14 -0300

    Correção InstituicaoController.php

--- app/Http/Controllers/InstituicaoController.php
+++ app/Http/Controllers/InstituicaoController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class InstituicoesController extends Controller
+class InstituicaoController extends Controller
 {
     public function __construct()
     {

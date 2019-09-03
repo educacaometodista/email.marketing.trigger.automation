5ec7e43138dcc3c958ad09bd07afc77c75591394
erickfirmo <erickluques@gmail.com>
2019-09-03 14:33:54 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ require 'auth/superadmin.php';
 
 Route::prefix('admin')->name('admin.')->group(function () {
     Route::prefix('listas')->name('listas.')->namespace('Emkt')->group(function () {
-        Route::resource('/importar', 'InstituicaoController');
+        Route::resource('/importar', 'EmktController');
     });
     Route::prefix('instituicoes')->name('instituicoes.')->group(function () {
         Route::resource('/instituicoes', 'InstituicaoController');

d4a1b7599270bc157da438006d951404753ccce0
erickfirmo <erickluques@gmail.com>
2019-09-03 13:55:44 -0300

    Exibição de alerts de status

--- resources/views/admin/emkt/listas/create.blade.php
+++ resources/views/admin/emkt/listas/create.blade.php
@@ -12,11 +12,23 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @if (session('message'))
-                        <div class="alert alert-darger" role="alert">
-                        {{ session('message') }}
-                        </div>
-                    @endif
+                    @foreach($instituicoes as $instituicao)
+                        @if(session('message-'.$instituicao))
+                            @if(str_replace($instituicao, '', session('message-'.$instituicao)) == 'Acesso negado em ')
+                                <div class="alert alert-danger" role="alert">
+                            @elseif(str_replace($instituicao, '', session('message-'.$instituicao)) == 'Processo iniciado em ')
+                                <div class="alert alert-success" role="alert">
+                            @endif
+
+                            {{ session('message-'.$instituicao) }}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                        @endif
+                    @endforeach
+
+
                     @include('admin.partials._alert')
                     <form action="{{ route('admin.listas.importar.store') }}" method="POST" enctype="multipart/form-data" class="col-md-8 mx-auto my-4">
                         @csrf

a0e13f1280ac6c8bcbf4f6399ddfd1d49045def9
erickfirmo <erickluques@gmail.com>
2019-09-04 16:07:26 -0300

    Correção na ordem de leitura do jquery

--- resources/views/layouts/dadmin.blade.php
+++ resources/views/layouts/dadmin.blade.php
@@ -13,7 +13,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <link rel="icon" href="{{ url('images/logo-icon.ico') }}">
+    <link rel="icon" href="{{ asset('images/logo-icon.ico') }}">
 
 
     <!-- ==== Document Title ==== -->
@@ -31,48 +31,53 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700%7CMontserrat:400,500">
 
     <!-- Stylesheets -->
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/fontawesome-all.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/jquery-ui.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/perfect-scrollbar.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/morris.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/jquery-jvectormap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/horizontal-timeline.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/weather-icons.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/dropzone.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/ion.rangeSlider.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/ion.rangeSlider.skinFlat.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/datatables.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/fullcalendar.min.css') }}">
-    <link rel="stylesheet" href="{{ url('vendor/dadmin/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/jquery-ui.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/perfect-scrollbar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/morris.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/jquery-jvectormap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/horizontal-timeline.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/weather-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/dropzone.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/ion.rangeSlider.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/ion.rangeSlider.skinFlat.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/datatables.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/fullcalendar.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/dadmin/assets/css/style.css') }}">
+    
 
     <!-- Page Level Stylesheets -->
 
 </head>
 <body>
 
-@yield('content')
+<!-- Wrapper Start -->
+<div class="wrapper">
+    @yield('content')
+</div>
 
 
 <!-- Scripts -->
-<script src="{{ url('vendor/dadmin/assets/js/jquery.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery-ui.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery.sparkline.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/raphael.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/morris.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/select2.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery-jvectormap.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery-jvectormap-world-mill.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/horizontal-timeline.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery.validate.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/jquery.steps.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/dropzone.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/ion.rangeSlider.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/datatables.min.js') }}"></script>
-<script src="{{ url('vendor/dadmin/assets/js/main.js') }}"></script>
+
+<script src="{{ asset('vendor/dadmin/assets/js/jquery-ui.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery.sparkline.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/raphael.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/morris.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/select2.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery-jvectormap.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery-jvectormap-world-mill.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/horizontal-timeline.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery.validate.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/jquery.steps.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/dropzone.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/ion.rangeSlider.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/datatables.min.js') }}"></script>
+<script src="{{ asset('vendor/dadmin/assets/js/main.js') }}"></script>
 
 <!-- Page Level Scripts -->
 

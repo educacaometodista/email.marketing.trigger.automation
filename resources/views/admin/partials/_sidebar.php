12a215fe7e5810a2f186351a9b16bc33562d855b
erickfirmo <erickluques@gmail.com>
2019-09-04 19:03:33 -0300

    Sidebar dashboard admin

---
+++ resources/views/admin/partials/_sidebar.php
@@ -0,0 +1,142 @@
+<!-- Sidebar Start -->
+<aside class="sidebar" data-trigger="scrollbar">
+    <!-- Sidebar Profile Start -->
+    <div class="sidebar--profile">
+        <div class="profile--img">
+            <a href="#">
+                <img src="#" alt="" class="rounded-circle">
+            </a>
+        </div>
+
+        <div class="profile--name">
+            <a href="#" class="btn-link">Educação Metodista</a>
+        </div>
+
+        <div class="profile--nav">
+            <ul class="nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link" title="User Profile">
+                        <i class="fa fa-user"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" title="Lock Screen">
+                        <i class="fa fa-lock"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" title="Messages">
+                        <i class="fa fa-envelope"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" title="Logout">
+                        <i class="fa fa-sign-out-alt"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Sidebar Profile End -->
+
+    <!-- Sidebar Navigation Start -->
+    <div class="sidebar--nav">
+        <ul>
+            <li>
+                <ul>
+                    <li class="active">
+                        <a href="#">
+                            <i class="fa fa-home"></i>
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li>
+                <a href="#">Cadastros</a>
+
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Instituições</span>
+                        </a>
+
+                        <ul>
+                            <li><a href="#">Todas</a></li>
+                            <li><a href="#">Novo Cadastro</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+            </li>
+
+            <li>
+                <a href="#">E-Mail Marketing</a>
+
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-th"></i>
+                            <span>Listas</span>
+                        </a>
+
+                        <ul>
+                            <li><a href="#">Todas</a></li>
+                            <li><a href="#">Importar Arquivo</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+    <!-- Sidebar Navigation End -->
+
+    <!-- Sidebar Widgets Start -->
+    <!--<div class="sidebar--widgets">
+        <div class="widget">
+            <h3 class="h6 widget--title">Information Summary</h3>
+
+            <div class="summary--widget">
+                <div class="summary--item">
+                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#2bb3c0">5,6,7,9,15,5,6,7,9,11,7,9,11,7,9,9,3,2</p>
+
+                    <p class="summary--title">Daily Traffic</p>
+                    <p class="summary--stats">307.512</p>
+                </div>
+
+                <div class="summary--item">
+                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#e16123">2,3,7,7,9,11,9,7,9,11,9,7,5,4,9,7,5,4</p>
+
+                    <p class="summary--title">Average Usage</p>
+                    <p class="summary--stats">2,371,527</p>
+                </div>
+
+                <div class="summary--item">
+                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#cccccc">5,6,7,9,15,5,6,7,9,11,7,9,11,7,9,9,3,2</p>
+
+                    <p class="summary--title">Disk Usage</p>
+                    <p class="summary--stats">37.5%</p>
+                </div>
+
+                <div class="summary--item">
+                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#009378">2,3,7,7,9,11,9,7,9,11,9,7,5,4,9,7,5,4</p>
+
+                    <p class="summary--title">CPU Usage</p>
+                    <p class="summary--stats">37.05-32</p>
+                </div>
+                
+                <div class="summary--item">
+                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#ff4040">5,6,7,9,15,5,6,7,9,11,7,9,11,7,9,9,3,2</p>
+
+                    <p class="summary--title">Memory Usage</p>
+                    <p class="summary--stats">37.05%</p>
+                </div>
+            </div>
+        </div>
+    </div>-->
+    <!-- Sidebar Widgets End -->
+</aside>
+<!-- Sidebar End -->

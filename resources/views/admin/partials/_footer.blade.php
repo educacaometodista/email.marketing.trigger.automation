0aafac9043feb155907fc7b1723e17f94dae2efc
erickfirmo <erickluques@gmail.com>
2019-09-04 19:15:10 -0300

    Footer dashboard admin

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<!-- Main Footer Start -->
+<footer class="main--footer main--footer-light">
+        <p>Copyright &copy; <a href="#">Educação Metodista</a>. Todos os Direitos Reservados.</p>
+    </footer>
+    <!-- Main Footer End -->

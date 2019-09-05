5a41fc21b0b3cd719ce29f1b73bf3d4d4923a76d
Erick <erickluques@gmail.com>
2019-09-05 17:26:47 -0300

    Correção na model Instituicao

---
+++ resources/views/admin/emkt/instituicoes/create.blade.php
@@ -0,0 +1,77 @@
+
+
+
+
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('admin.partials._navbar')
+@include('admin.partials._sidebar')
+
+<!-- Main Container Start -->
+<main class="main--container">
+    <!-- Page Header Start -->
+    <section class="page--header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Page Title Start -->
+                    <h2 class="page--title h5">Instituições</h2>
+                    <!-- Page Title End -->
+
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item active"><span>Novo Cadastro</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Page Header End -->
+
+
+    <!-- Main Content Start -->
+    <section class="main--content">
+        <div class="row gutter-20">
+            <div class="col-md-12">
+                <!-- Panel Start -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Novo Cadastro</h3>
+                    </div>
+
+                    <div class="panel-content">
+                    @include('admin.partials._alert')
+                    
+                        <form action="{{ route('admin.instituicoes.store') }}" method="POST">
+                            @csrf
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                    <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
+                                <div class="col-md-10">
+                                    <input type="text" name="nome" class="form-control" id="nome">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <div class="row">
+                                <div class="col-lg-10 offset-lg-2">
+                                    
+                                    <input type="submit" value="Importar" class="btn btn-sm btn-rounded btn-success">
+                                    <button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <!-- Panel End -->
+            </div>
+        </div>
+    </section>
+
+    @include('admin.partials._footer')
+</main>
+<!-- Main Container End -->
+@endsection
+

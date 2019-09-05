da9f8044486e07f87377a714e728aae49a916664
Erick <erickluques@gmail.com>
2019-09-05 17:55:11 -0300

    Método store InstituicaoController

--- app/Http/Controllers/InstituicaoController.php
+++ app/Http/Controllers/InstituicaoController.php
@@ -2,13 +2,87 @@
 
 namespace App\Http\Controllers;
 
+use App\Instituicao;
 use Illuminate\Http\Request;
 
 class InstituicaoController extends Controller
 {
-    public function __construct()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
     {
-        $this->middleware('auth:admin');
+        //
     }
-    
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $instituicao = new Instituicao;
+        $instituicao->nome = $request->input('nome');
+        $instituicao->codigo_da_empresa = $request->input('codigo_da_empresa');
+        $instituicao->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Instituicao  $instituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Instituicao $instituicao)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Instituicao  $instituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Instituicao $instituicao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Instituicao  $instituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Instituicao $instituicao)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Instituicao  $instituicao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Instituicao $instituicao)
+    {
+        //
+    }
 }

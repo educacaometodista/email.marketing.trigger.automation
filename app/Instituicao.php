50387f72fa4679b7a2f3bfb80f8492e2f4622845
Érick Firmo <34639603+erickfirmo@users.noreply.github.com>
2019-09-06 01:24:20 -0300

    Update Instituicao.php

--- app/Instituicao.php
+++ app/Instituicao.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Instituicao extends Model
 {
-    protected $guard = 'instituicoes';
+    protected $table = 'instituicoes';
 
     protected $fillable = [
         'nome',

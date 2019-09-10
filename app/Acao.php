f73c32ffca07f7c7b115b9da7e024becc36f88ce
erickfirmo <erickluques@gmail.com>
2019-09-10 17:16:29 -0300

    Update Acao.php

--- app/Acao.php
+++ app/Acao.php
@@ -13,9 +13,6 @@ class Acao extends Model
         'envio',
         'destinatarios',
         'status',
-        'nome_do_remetente',
-        'email_do_remetente',
-        'email_de_retorno',
         'mensagem_id'
     ];
 }

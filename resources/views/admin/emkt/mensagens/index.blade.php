3bc8afa594dcd65e04b18490e27c61ec99e1369a
erickfirmo <erickluques@gmail.com>
2019-09-11 12:43:31 -0300

    Create mensagens/index.blade.php

---
+++ resources/views/admin/emkt/mensagens/index.blade.php
@@ -0,0 +1,125 @@
+
+
+
+
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('admin.partials._navbar')
+@include('admin.partials._sidebar')
+<!-- Main Container Start -->
+<main class="main--container">
+            <!-- Page Header Start -->
+            <section class="page--header">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Page Title Start -->
+                            <h2 class="page--title h5">Cadastros</h2>
+                            <!-- Page Title End -->
+
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('admin.mensagens.index') }}">Mensagem</a></li>
+                                <li class="breadcrumb-item active"><span>Todas</span></li>
+                            </ul>
+                        </div>
+
+                        <!--<div class="col-lg-6">
+                            <div class="summary--widget">
+                                <div class="summary--item">
+                                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#009378">2,9,7,9,11,9,7,5,7,7,9,11</p>
+
+                                    <p class="summary--title">This Month</p>
+                                    <p class="summary--stats text-green">2,371,527</p>
+                                </div>
+
+                                <div class="summary--item">
+                                    <p class="summary--chart" data-trigger="sparkline" data-type="bar" data-width="5" data-height="38" data-color="#e16123">2,3,7,7,9,11,9,7,9,11,9,7</p>
+
+                                    <p class="summary--title">Last Month</p>
+                                    <p class="summary--stats text-orange">2,527,371</p>
+                                </div>
+                            </div>
+                        </div>-->
+                    </div>
+                </div>
+            </section>
+            <!-- Page Header End -->
+
+            <!-- Main Content Start -->
+            <section class="main--content">
+                <div class="panel">
+                    <!-- Records Header Start -->
+                    <div class="records--header">
+                        <div class="title fa-envelope">
+                            <h3 class="h3">Mensagens 
+                            <!--<a href="#" class="btn btn-sm btn-outline-info">Manage Orders</a>-->
+                        
+                            </h3>
+                            <p>{{ count($mensagens) == 1 ? count($mensagens).' Mensagem encontrada' : count($mensagens).' Mensagens encontradas' }}</p>
+                        </div>
+
+                        <!--<div class="actions">
+                            <form action="#" class="search">
+                                <input type="text" class="form-control" placeholder="Order ID or Customer Name..." required>
+                                <button type="submit" class="btn btn-rounded"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>-->
+                    </div>
+                    <!-- Records Header End -->
+                </div>
+
+                @include('admin.partials._alert')
+
+
+                <div class="panel">
+
+                    <div class="records--list" data-title="Lista de mensagens">
+                        <table id="recordsListView">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Título</th>
+                                    <th>URL</th>
+                                    <th class="not-sortable">Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($mensagens as $mensagem)
+
+                                <tr id="{{ 'mensagem-'.$mensagem->id }}">
+                                    <td>{{ $mensagem->id }}</td>
+                                    <td>{{ $mensagem->titulo }}</td>
+                                    <td>{{ $mensagem->url }}</td>
+                                    <td>
+                                        <div class="dropleft">
+                                            <a href="#" class="btn-link" data-toggle="dropdown"><i class="fa fa-ellipsis-v"></i></a>
+
+                                            <div class="dropdown-menu">
+                                                <a href="{{ route('admin.mensagens.edit', $mensagem->id ) }}" class="dropdown-item">Editar</a>
+                                                <form action="{{ route('admin.mensagens.destroy', $mensagem->id) }}" method="POST">
+                                                    @csrf
+                                                    {{method_field('DELETE')}}
+                                                    <button class="dropdown-item btn-remove">Excluir</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- Records List End -->
+                </div>
+            </section>
+            <!-- Main Content End -->
+
+    @include('admin.partials._footer')
+</main>
+<!-- Main Container End -->
+
+
+@endsection
+

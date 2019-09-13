65a85b62af06a4dacc975db78d1380fad7bbc00f
erickfirmo <erickluques@gmail.com>
2019-09-13 16:41:13 -0300

    Separação do método de importação de listas + retorno dos nomes das listas

--- app/Http/Controllers/Emkt/ListaController.php
+++ app/Http/Controllers/Emkt/ListaController.php
@@ -38,39 +38,48 @@ class ListaController extends Controller
     {
         if($request->hasFile('import_file'))
         {
-            if(isset($this->instituicoes))
-            {
-                $extension = 'csv';
-                $subject = $request->input('subject');
-                $date = date('d-m', strtotime($request->input('date')));
-                $period = date('Y', strtotime($request->input('date')));
+            $extension = 'csv';
+            $subject = $request->input('subject');
+            $date = date('d-m-Y', strtotime($request->input('date')));
+            $currentFile = $this->planilha()->load($request->file('import_file')->getRealPath());
+            $hasAction = false;
 
-                $explode_date = explode('-', $date);
-                $month = $explode_date[1];
-                $period .= $month >=7 ? '-2' : '';
-                
-                $currentFile = $this->planilha()->load($request->file('import_file')->getRealPath());
-                
-                $this->planilha()->filter($currentFile, $extension, str_replace(' ', '-', strtolower($subject)), $date.'-'.$period, 'akna_lists');
-
-                $codigos_dos_processos = [];
-            
-                foreach ($this->instituicoes as $codigo_da_empresa => $instituicao)
-                {
-                    $nome_do_arquivo = strtolower($this->prefixo[$instituicao]).'-'.strtolower($subject).'-'.$date.'-'.$period.'.'.$extension;
-                    $nome_da_lista = 'teste-'.ucwords($this->prefixo[$instituicao]).' - '.str_replace('-', ' ', $subject).' - '.str_replace('-', '/', $date).' - '.str_replace('-', '/',$period);
-                    Session::flash('message-'.$this->prefixo[$instituicao], $this->aknaAPI()->importarListaDeContatos($nome_da_lista, $nome_do_arquivo, $instituicao, $codigo_da_empresa));
-                }
-
-                return back();
-
-            } else {
-
-                return back()->with('warning', 'Não há instituições cadastradas para importar este arquivo!');
-            }
+            return $this->import($currentFile, $extension, $subject, $date, $hasAction);
 
         } else {
             return back()->with('message', 'Você precisa importar um documento!');
         }
+    }
+
+    public function import($currentFile, $extension, $subject, $date, $hasAction)
+    {
+        $explode_date = explode('/', $date);
+
+        $day = $explode_date[0];
+        $month = $explode_date[1];
+        $period = $explode_date[2];
+        $period .= $month >=7 ? '-2' : '';
+
+        if(isset($this->instituicoes))
+        {
+            $this->planilha()->filter($currentFile, $extension, str_replace(' ', '-', strtolower($subject)), $day.'-'.$month.'-'.$period, 'akna_lists');
+
+            $codigos_dos_processos = [];
+            $nomes_das_listas = [];
+        
+            foreach ($this->instituicoes as $codigo_da_empresa => $instituicao)
+            {
+                $nome_do_arquivo = strtolower($this->prefixo[$instituicao]).'-'.strtolower($subject).'-'.$day.'-'.$month.'-'.$period.'.'.$extension;
+                $nome_da_lista = 'teste-'.ucwords($this->prefixo[$instituicao]).' - '.str_replace('-', ' ', $subject).' - '.$day.'/'.$month.' - '.str_replace('-', '/',$period);
+                Session::flash('message-'.$this->prefixo[$instituicao], $this->aknaAPI()->importarListaDeContatos($nome_da_lista, $nome_do_arquivo, $instituicao, $codigo_da_empresa));
+                $nomes_das_listas[$this->prefixo[$instituicao]] = $nome_da_lista;
+            }
+
+            return $hasAction == true ? $nomes_das_listas : back();
+
+        } else {
+
+            return back()->with('warning', 'Não há instituições cadastradas para importar este arquivo!');
+        }   
     }
 }

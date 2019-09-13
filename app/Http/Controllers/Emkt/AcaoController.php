fcdd7e05a8cd291c1ad8f3d0488c04d15c857426
erickfirmo <erickluques@gmail.com>
2019-09-13 18:56:41 -0300

    Exclusão de métodos não utilizados em AcaoController

--- app/Http/Controllers/Emkt/AcaoController.php
+++ app/Http/Controllers/Emkt/AcaoController.php
@@ -64,50 +64,9 @@ class AcaoController extends Controller
         return view('admin.emkt.acao.show', ['acao' => Acao::findOrFail($id)]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Acao  $acao
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        return view('admin.emkt.acoes.edit', ['acao' => Acao::findOrFail($id)]);
-    }
+    
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Acao  $acao
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $acao = Acao::findOrFail($id)->update([
-            'titulo' => $request->titulo,
-            'envio' => $request->envio,
-            'destinatarios' -> $request->destinatarios,
-            'status' => $request->status,
-            'mensagem_id' => $request->mensagem_id
-        ]);
+    
 
-        return redirect()->route('admin.acoes.edit', compact('acao'))
-            ->with('success', 'Ação atualizada com sucesso!');
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Acao  $acao
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        Acao::findOrFail($id)->delete();
-
-        return redirect()->route('admin.acoes.index')
-            ->with('success', 'Ação removida com sucesso!');
-    }
+    
 }

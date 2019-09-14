fde4c41ef5179f97cc7042f3810ffc1f31fbcb75
Erick <erickluques@gmail.com>
2019-09-14 20:53:57 -0300

    Alteração de namespace

--- app/Http/Controllers/Emkt/InstituicaoController.php
+++ app/Http/Controllers/Emkt/InstituicaoController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Emkt;
 
 use App\Instituicao;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class InstituicaoController extends Controller
 {

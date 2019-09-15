c517388707daca9f29a52d23e28cba55b9870b19
Erick <erickluques@gmail.com>
2019-09-15 06:07:44 -0300

    Create Lista.php

---
+++ app/Lista.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lista extends Model
+{
+    public $table = 'listas';
+
+    public $fillable = [
+        'nome_da_lista',
+        'nome_do_arquivo',
+        'conteudo',
+        'instituicao_id',
+    ];
+}

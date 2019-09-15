e4035f23b133d12f451b4b7ece6d16e17ecb5a9d
Erick <erickluques@gmail.com>
2019-09-15 06:08:37 -0300

    Create 2019_09_15_081912_create_listas_table.php

---
+++ database/migrations/2019_09_15_081912_create_listas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome_da_lista');
+            $table->string('nome_do_arquivo');
+            $table->string('conteudo');
+            $table->unsignedInteger('instituicao_id');
+            $table->timestamps();
+
+            $table->foreign('instituicao_id')
+                ->references('id')
+                ->on('instituicoes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listas');
+    }
+}

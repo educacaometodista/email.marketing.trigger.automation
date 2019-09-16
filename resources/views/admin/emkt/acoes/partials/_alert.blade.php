bce876149639ebc333340d3d3643bf05301be397
Érick Firmo <34639603+erickfirmo@users.noreply.github.com>
2019-09-16 19:33:18 -0300

    Update _alert.blade.php

--- resources/views/admin/emkt/acoes/partials/_alert.blade.php
+++ resources/views/admin/emkt/acoes/partials/_alert.blade.php
@@ -1,8 +1,8 @@
 @if(isset($instituicoes))
     @foreach($instituicoes as $instituicao)
-        @if(session('message-'.$instituicao->prefixo))
+        @if(session('message-success-'.$instituicao->prefixo))
             <div class="alert alert-success" role="alert">
-                {{ session('message-'.$instituicao->prefixo) }}
+                {{ session('message-success-'.$instituicao->prefixo) }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
@@ -10,3 +10,17 @@
         @endif
     @endforeach
 @endif
+
+@if(isset($instituicoes))
+    @foreach($instituicoes as $instituicao)
+        @if(session('message-danger-'.$instituicao->prefixo))
+            <div class="alert alert-danger" role="alert">
+                {{ session('message-danger-'.$instituicao->prefixo) }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+    @endforeach
+@endif
+

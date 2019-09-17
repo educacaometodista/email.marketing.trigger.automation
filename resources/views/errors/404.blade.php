75602c4b08bb6c2e8845f8cf0ab0b52985acb2d4
Léu Almeida <leo@webid.net.br>
2019-09-17 17:20:34 -0300

    Criação de nova página de Erro 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.dadmin')
+
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+        <!-- Error Page Start -->
+        <div class="m-error m-error-404">
+            <div class="m-error--content">
+                <div class="m-error--title">
+                    <h1 class="h1">404</h1>
+                </div>
+
+                <div class="m-error--desc">
+                    <h2 class="h2">Oops! Algo deu errado</h2>
+                    <p>Pedimos desculpas, mas não foi possível encontrar a página que você estava esperando...</p>
+
+                    <div class="m-error--search">
+                        <form action="#" method="get">
+
+                            <a href="{{ route('home') }}" class="btn btn-block btn-rounded btn-info">Ir para o início</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Error Page End -->
+    </div>
+    <!-- Wrapper End -->
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery-ui.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/perfect-scrollbar.min.js"></script>
+    <script src="assets/js/jquery.sparkline.min.js"></script>
+    <script src="assets/js/raphael.min.js"></script>
+    <script src="assets/js/morris.min.js"></script>
+    <script src="assets/js/select2.min.js"></script>
+    <script src="assets/js/jquery-jvectormap.min.js"></script>
+    <script src="assets/js/jquery-jvectormap-world-mill.min.js"></script>
+    <script src="assets/js/horizontal-timeline.min.js"></script>
+    <script src="assets/js/jquery.validate.min.js"></script>
+    <script src="assets/js/jquery.steps.min.js"></script>
+    <script src="assets/js/dropzone.min.js"></script>
+    <script src="assets/js/ion.rangeSlider.min.js"></script>
+    <script src="assets/js/datatables.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+    <!-- Page Level Scripts -->
+
+</body>
+</html>

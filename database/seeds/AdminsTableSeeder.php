b18ef930d4344c0f615d3b14689b78b194b0ee58
erickfirmo <erickluques@gmail.com>
2019-09-19 13:10:06 -0300

    Fotos de perfil em AdminsTableSeeder

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -13,6 +13,7 @@ class AdminsTableSeeder extends Seeder
         $admin = new App\Admin;
         $admin->name = 'Leonardo Almeida';
         $admin->email = 'leonardo.almeida@metodista.br';
+        $admin->foto_de_perfil = 'https://avatars3.githubusercontent.com/u/42948574?s=460&v=4';
         $admin->password = bcrypt('psw');
         $admin->remember_token = str_random(10);
         $admin->save();
@@ -20,6 +21,7 @@ class AdminsTableSeeder extends Seeder
         $admin = new App\Admin;
         $admin->name = 'Thiago Tamosauskas';
         $admin->email = 'thiago.tamosauskas@metodista.br';
+        $admin->foto_de_perfil = 'https://avatars2.githubusercontent.com/u/12496600?s=460&v=4';
         $admin->password = bcrypt('psw');
         $admin->remember_token = str_random(10);
         $admin->save();
@@ -27,6 +29,7 @@ class AdminsTableSeeder extends Seeder
         $admin = new App\Admin;
         $admin->name = 'Erick Firmo';
         $admin->email = 'erick.serrano@metodista.br';
+        $admin->foto_de_perfil = 'https://avatars3.githubusercontent.com/u/34639603?s=460&v=4';
         $admin->password = bcrypt('psw');
         $admin->remember_token = str_random(10);
         $admin->save();
@@ -34,6 +37,7 @@ class AdminsTableSeeder extends Seeder
         $admin = new App\Admin;
         $admin->name = 'Fernando Zancopé';
         $admin->email = 'fernando.zancope@metodista.br';
+        $admin->foto_de_perfil = 'https://avatars0.githubusercontent.com/u/53548067?s=460&v=4';
         $admin->password = bcrypt('psw');
         $admin->remember_token = str_random(10);
         $admin->save();

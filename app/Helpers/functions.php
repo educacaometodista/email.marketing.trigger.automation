590a29bdc10a048a8454cc92f75821e61ddf09d3
erickfirmo <erickluques@gmail.com>
2019-09-19 19:32:39 -0300

    Create functions.php - helper classActivePath

---
+++ app/Helpers/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+function classActivePath($path, $activeClass, $n)
+{
+    if($n != 0) {
+        $i = 0;
+        $final_path = '';
+        $explode_path = explode('/', $_SERVER['REQUEST_URI']);
+        while($n > $i)
+        {
+            $i++;
+            $final_path .= '/'.$explode_path[$i];
+        }
+        if($path == $final_path)
+            echo " $activeClass";
+
+    } else {
+        if($_SERVER['REQUEST_URI'] == $path)
+            echo " $activeClass";
+    }
+}

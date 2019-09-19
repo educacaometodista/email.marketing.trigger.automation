e12f83b07b5fd5c04dc194fe9223c3e0499ba616
Léu Almeida <leo@webid.net.br>
2019-09-19 15:30:06 -0300

    Atualização no Rodapé (URL do GitHub)

--- resources/views/superadmin/partials/_footer.blade.php
+++ resources/views/superadmin/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Footer Start -->
 <footer class="main--footer main--footer-light">
-        <p>Copyright &copy; <a href="#">Educação Metodista</a>. Todos os Direitos Reservados.</p>
+        <p>Copyright &copy; <a href="https://github.com/educacaometodista">Educação Metodista</a>. Todos os Direitos Reservados.</p>
     </footer>
     <!-- Main Footer End -->

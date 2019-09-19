29bb25178840583d1359b2a1358935c3544b6a43
erickfirmo <erickluques@gmail.com>
2019-09-19 12:54:22 -0300

    Campo 'foto_de_perfil' na Model Admin

--- app/Admin.php
+++ app/Admin.php
@@ -21,7 +21,7 @@ class Admin extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'foto_de_perfil', 'password',
     ];
 
     /**

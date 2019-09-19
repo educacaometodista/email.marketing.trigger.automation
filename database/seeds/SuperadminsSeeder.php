01b98cdc21a8b5984aa7f14db6457e9f0b402c83
erickfirmo <erickluques@gmail.com>
2019-09-19 13:10:25 -0300

    Foto de perfil em SuperadminsTableSeeder

--- database/seeds/SuperadminsSeeder.php
+++ database/seeds/SuperadminsSeeder.php
@@ -13,6 +13,7 @@ class SuperadminsTableSeeder extends Seeder
         $superadmin = new App\Superadmin;
         $superadmin->name = 'Erick Firmo';
         $superadmin->email = 'erick.serrano@metodista.br';
+        $superadmin->foto_de_perfil = 'https://avatars3.githubusercontent.com/u/53944427?s=200&v=4';
         $superadmin->password = bcrypt('*Secret2019');
         $superadmin->remember_token = str_random(10);
         $superadmin->save();

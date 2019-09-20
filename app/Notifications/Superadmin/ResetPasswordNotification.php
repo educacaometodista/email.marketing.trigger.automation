11bace401d1a6e988eaca795d0671044a72efd4a
erickfirmo <erickluques@gmail.com>
2019-09-20 14:48:36 -0300

    Update ResetPasswordNotification.php

--- app/Notifications/Superadmin/ResetPasswordNotification.php
+++ app/Notifications/Superadmin/ResetPasswordNotification.php
@@ -18,7 +18,7 @@ class ResetPasswordNotification extends Notification
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($token)
     {
         $this->token = $token;
     }

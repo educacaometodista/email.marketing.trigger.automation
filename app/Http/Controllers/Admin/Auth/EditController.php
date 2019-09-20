656b43ce88bdeab7ad29d9f105bb16bd6357b9ba
erickfirmo <erickluques@gmail.com>
2019-09-20 18:37:20 -0300

    Update EditController.php

--- app/Http/Controllers/Admin/Auth/EditController.php
+++ app/Http/Controllers/Admin/Auth/EditController.php
@@ -17,11 +17,16 @@ class EditController extends Controller
 
     public function edit($id)
     {
-        return view('admin.auth.edit', ['admin' => Admin::findOrfail($id)]);
+        if($id == Auth::user()->id)
+            return view('admin.auth.edit', ['admin' => Admin::findOrfail($id)]);
+        else
+            return redirect()->route('admin.home');
+
     }
 
     public function update(Request $request, $id)
     {
+        
         $request->validate([
             'name' => 'required|min:2|max:255|string',
             'password' => 'required|min:6|max:255|string',
@@ -36,6 +41,8 @@ class EditController extends Controller
 
         return redirect()->route('admin.edit', compact('admin'))
             ->with('success', 'Informações alteradas com sucesso!');
+
+        
     }
 
 }

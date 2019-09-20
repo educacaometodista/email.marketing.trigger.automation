e286e20471cad9c2e05af64dd4f54d51d30c8742
erickfirmo <erickluques@gmail.com>
2019-09-20 15:55:22 -0300

    Update user.php

--- routes/auth/user.php
+++ routes/auth/user.php
@@ -7,4 +7,10 @@ Route::prefix('user')->name('user.')->namespace('User')->group(function () {
         Route::get('/', 'HomeController@home')->name('/');
         Route::get('home', 'HomeController@index')->name('home');
     });
+    Route::get('edit/{id}', 'Auth\EditController@edit')->name('edit');
+    Route::post('update/{id}', 'Auth\EditController@update')->name('update');
+
+    Route::get('/password/reset', function () {
+        return redirect('user/login');
+    });
 });

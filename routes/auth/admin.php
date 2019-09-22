976f5fd21b4a7e7a17077e4d214a7b74f12efdf5
Erick <erickluques@gmail.com>
2019-09-21 21:59:52 -0300

    Update admin.php

--- routes/auth/admin.php
+++ routes/auth/admin.php
@@ -4,7 +4,7 @@ Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
     Auth::routes();
     Route::middleware('auth:admin')->group(function () {
         Route::get('/', 'HomeController@index')->name('/');
-        Route::get('home', 'HomeController@index')->name('home');
+        Route::get('home', 'HomeController@home')->name('home');
     });
     Route::get('edit/{id}', 'Auth\EditController@edit')->name('edit');
     Route::post('update/{id}', 'Auth\EditController@update')->name('update');

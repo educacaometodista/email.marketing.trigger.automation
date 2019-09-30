c8b2e430008585b4a6d06130e56ac8049a2f0c3a
Erick <erickluques@gmail.com>
2019-09-29 22:12:04 -0300

    Update acoes/create.blade.php

--- resources/views/admin/emkt/acoes/create.blade.php
+++ resources/views/admin/emkt/acoes/create.blade.php
@@ -58,8 +58,8 @@
                                 <div class="col-md-10">
                                     <select name="tipo_de_acao" class="form-control" id="tipo_de_acao">
                                         <option></option>
-                                        @foreach($tipos_de_acoes as $key => $tipo_de_acao)
-                                            <option value="{{ $key }}">{{ $tipo_de_acao }}</option>
+                                        @foreach($tipos_de_acoes as $tipo_de_acao)
+                                            <option value="{{ $tipo_de_acao->nome }}">{{ $tipo_de_acao->nome_de_exibicao }}</option>
                                         @endforeach
                                     </select>
                                 </div>

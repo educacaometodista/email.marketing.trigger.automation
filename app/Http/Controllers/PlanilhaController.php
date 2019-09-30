16a91107f713e769d3f7cc6f048d76df29fbacf3
Erick <erickluques@gmail.com>
2019-09-30 01:08:49 -0300

    Substituição de assunto por tipo de ação na importação de planilhas de contatos

--- app/Http/Controllers/PlanilhaController.php
+++ app/Http/Controllers/PlanilhaController.php
@@ -60,14 +60,14 @@ class PlanilhaController extends Controller
         }
     }
 
-    public function validator($subject, $currentFile)
+    public function validator($tipo_de_acao, $currentFile)
     {
         $currentFile = is_object($currentFile) ? $currentFile->toArray() : (is_array($currentFile) ? $currentFile : null);
 
-        if($subject == 'ausentes' || $subject == 'inscritos-parciais' || $subject == 'lembrete-de-prova' || $subject == 'aprovados-não-matriculados')
+        if($tipo_de_acao == 'ausentes' || $tipo_de_acao == 'inscritos-parciais' || $tipo_de_acao == 'lembrete-de-prova' || $tipo_de_acao == 'aprovados-nao-matriculados')
             $this->filter_name = 'Presencial';
 
-        elseif($subject == 'ausentes-a-distancia' || $subject == 'inscritos-parciais-a-distancia' || $subject == 'lembrete-de-prova-a-distancia' || $subject == 'aprovados-não-matriculados-a-distancia')
+        elseif($tipo_de_acao == 'ausentes-a-distancia' || $tipo_de_acao == 'inscritos-parciais-a-distancia' || $tipo_de_acao == 'lembrete-de-prova-a-distancia' || $tipo_de_acao == 'aprovados-nao-matriculados-a-distancia')
             $this->filter_name = 'Ead';
         
         if($this->filter_name == 'Ead')
@@ -104,175 +104,38 @@ class PlanilhaController extends Controller
         return true;
     }
 
-    public function getFilter($currentFile, $extension, $subject, $date, $storage_path)
+    public function getFilter($currentFile, $extension, $tipo_de_acao, $date, $storage_path)
     {
         if($this->filter_name == 'Ead')
         {
-            $this->filtroEad($currentFile, $extension, $subject, $date, $storage_path);
+            $this->filtroEad($currentFile, $extension, $tipo_de_acao, $date, $storage_path);
 
         } elseif($this->filter_name == 'Presencial') {
 
-            $this->filtroPresencial($currentFile, $extension, $subject, $date, $storage_path);
+            $this->filtroPresencial($currentFile, $extension, $tipo_de_acao, $date, $storage_path);
 
         }
     }
 
-    public function filter($currentFile, $extension, $subject, $date, $storage_path)
+    public function filter($currentFile, $extension, $tipo_de_acao, $date, $storage_path)
     {
 
-        if(!$this->validator($subject, $currentFile)) {
+        if(!$this->validator($tipo_de_acao, $currentFile)) {
             return back();
         }
 
-        $subject = strtolower($subject);
+        $tipo_de_acao = strtolower($tipo_de_acao);
         
         $currentFile = $currentFile->toArray();
         
-        $this->getFilter($currentFile, $extension, $subject, $date, $storage_path);
+        $this->getFilter($currentFile, $extension, $tipo_de_acao, $date, $storage_path);
        
     }
 
-    public function filtroPresencial($currentFile, $extension, $subject, $date, $storage_path)
+    public function filtroPresencial($currentFile, $extension, $tipo_de_acao, $date, $storage_path)
     {
-        $document_hash = md5($subject.$date.date('d-m-y h:i:s'));
+        $document_hash = md5($tipo_de_acao.$date.date('d-m-y h:i:s'));
 
-        $headers = [];
-
-        if(array_key_exists('nome', $currentFile[0]) && array_key_exists('e_mail', $currentFile[0]) && array_key_exists('celular', $currentFile[0]) && array_key_exists('instituição', $currentFile[0]))
-        {
-
-            foreach($currentFile[0] as $header => $value)
-                array_push($headers, $header);
-            
-            foreach($currentFile as $key_row => $row)
-            {
-                
-                foreach($row as $key => $cell)
-                {
-                    if($key == $headers[2])
-                    {
-                        $currentFile[$key_row][$key] = str_replace('-', '', $currentFile[$key_row][$key]);
-                        $currentFile[$key_row][$key] = str_replace('(', '', $currentFile[$key_row][$key]);
-                        $currentFile[$key_row][$key] = str_replace(')', '', $currentFile[$key_row][$key]);
-                        $currentFile[$key_row][$key] = str_replace(' ', '', $currentFile[$key_row][$key]);
-
-                        if(strlen($currentFile[$key_row][$key]) != 11)
-                            if(isset($currentFile[$key_row][$key]))
-                                $currentFile[$key_row][$key] = '';
-                    }
-
-                    if(!($key == $headers[0] || $key == $headers[1] || $key == $headers[2] || $key == $headers[17]))
-                        unset($currentFile[$key_row][$key]);
-
-                }
-                
-                $planilha = new Planilha;
-                $planilha->nome = $currentFile[$key_row][$headers[0]];
-                $planilha->email = $currentFile[$key_row][$headers[1]];
-                $planilha->celular = ($currentFile[$key_row][$headers[2]]) ? $currentFile[$key_row][$headers[2]] : '';
-                $planilha->instituicao = $currentFile[$key_row][$headers[17]];
-                $planilha->documento = $document_hash;
-                $planilha->save();
-            }
-
-            Session::put('zipName', "$subject-$date");
-
-            $currentFile = Planilha::select('nome', 'email', 'celular', 'instituicao')
-                                    ->where('documento', '=', $document_hash)
-                                    ->orderBy('instituicao')
-                                    ->orderBy('nome')
-                                    ->get()
-                                    ->toArray();
-
-            $umesp_file = [];
-            $unimep_file = [];
-            $izabela_file = [];
-            $granbery_file = [];
-            $fames_file = [];
-            $ipa_file = [];
-
-            foreach($currentFile as $row)
-            {
-                switch ($row['instituicao']) {
-                    case 'Umesp':
-                        array_push($umesp_file, $row);
-                        break;
-
-                    case 'Unimep':
-                        array_push($unimep_file, $row);
-                        break;
-
-                    case 'Izabela':
-                        array_push($izabela_file, $row);
-                        break;
-
-                    case 'Granbery':
-                        array_push($granbery_file, $row);
-                        break;
-
-                    case 'Fames':
-                        array_push($fames_file, $row);
-                        break;
-                        
-                    case 'Ipa':
-                        array_push($ipa_file, $row);
-                        break;
-                }
-            }
-
-            $this->clearStorage($storage_path);
-
-            $file_list = [];
-
-            if(count($umesp_file) > 0)
-            {
-                $this->storeFile($umesp_file, 'umesp-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'umesp-'.$subject.'-'.$date);
-            }
-
-            if(count($unimep_file) > 0)
-            {
-                $this->storeFile($unimep_file, 'unimep-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'unimep-'.$subject.'-'.$date);
-            }
-                
-            if(count($izabela_file) > 0)
-            {
-                $this->storeFile($izabela_file, 'izabela-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'izabela-'.$subject.'-'.$date);
-            }
-
-            if(count($granbery_file) > 0)
-            {
-                $this->storeFile($granbery_file, 'granbery-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'granbery-'.$subject.'-'.$date);
-            }
-
-            if(count($fames_file) > 0)
-            {
-                $this->storeFile($fames_file, 'fames-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'fames-'.$subject.'-'.$date);
-            }
-                
-            if(count($ipa_file) > 0)
-            {
-                $this->storeFile($ipa_file, 'ipa-'.$subject.'-'.$date, $extension, public_path($storage_path));
-                array_push($file_list, 'ipa-'.$subject.'-'.$date);
-            }
-
-            return $file_list;
-        } else {
-
-            return back()->with('message', 'Este documento não pode ser tratado!');
-        }
-
-    }
-
-    public function filtroEad($currentFile, $extension, $subject, $date, $storage_path)
-    {
-
-        $subject = str_replace('-a-distancia', '', $subject);
-        $document_hash = md5($subject.$date.date('d-m-y h:i:s'));
         $headers = [];
 
         foreach($currentFile[0] as $header => $value)
@@ -280,21 +143,36 @@ class PlanilhaController extends Controller
         
         foreach($currentFile as $key_row => $row)
         {
+            
             foreach($row as $key => $cell)
-                if(!($key == 'nome' || $key == 'e_mail' || $key == 'número' || $key == 'ddd'))
-                        unset($currentFile[$key_row][$key]);               
+            {
+                if($key == $headers[2])
+                {
+                    $currentFile[$key_row][$key] = str_replace('-', '', $currentFile[$key_row][$key]);
+                    $currentFile[$key_row][$key] = str_replace('(', '', $currentFile[$key_row][$key]);
+                    $currentFile[$key_row][$key] = str_replace(')', '', $currentFile[$key_row][$key]);
+                    $currentFile[$key_row][$key] = str_replace(' ', '', $currentFile[$key_row][$key]);
+
+                    if(strlen($currentFile[$key_row][$key]) != 11)
+                        if(isset($currentFile[$key_row][$key]))
+                            $currentFile[$key_row][$key] = '';
+                }
+
+                if(!($key == $headers[0] || $key == $headers[1] || $key == $headers[2] || $key == $headers[17]))
+                    unset($currentFile[$key_row][$key]);
+
+            }
             
             $planilha = new Planilha;
-            $planilha->nome = $currentFile[$key_row]['nome'];
-            $planilha->email = $currentFile[$key_row]['e_mail'];
-            $planilha->celular = ($currentFile[$key_row]['número'] && $currentFile[$key_row]['ddd']) ? $currentFile[$key_row]['ddd'].$currentFile[$key_row]['número'] : '';
-            $planilha->instituicao = 'EaD-UMESP';
+            $planilha->nome = $currentFile[$key_row][$headers[0]];
+            $planilha->email = $currentFile[$key_row][$headers[1]];
+            $planilha->celular = ($currentFile[$key_row][$headers[2]]) ? $currentFile[$key_row][$headers[2]] : '';
+            $planilha->instituicao = $currentFile[$key_row][$headers[17]];
             $planilha->documento = $document_hash;
-
             $planilha->save();
         }
 
-        Session::put('zipName', "$subject-$date");
+        Session::put('zipName', "$tipo_de_acao-$date");
 
         $currentFile = Planilha::select('nome', 'email', 'celular', 'instituicao')
                                 ->where('documento', '=', $document_hash)
@@ -345,38 +223,153 @@ class PlanilhaController extends Controller
 
         if(count($umesp_file) > 0)
         {
-            $this->storeFile($umesp_file, 'umesp-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'umesp-'.$subject.'-'.$date);
+            $this->storeFile($umesp_file, 'umesp-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'umesp-'.$tipo_de_acao.'-'.$date);
         }
 
         if(count($unimep_file) > 0)
         {
-            $this->storeFile($unimep_file, 'unimep-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'unimep-'.$subject.'-'.$date);
+            $this->storeFile($unimep_file, 'unimep-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'unimep-'.$tipo_de_acao.'-'.$date);
         }
             
         if(count($izabela_file) > 0)
         {
-            $this->storeFile($izabela_file, 'izabela-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'izabela-'.$subject.'-'.$date);
+            $this->storeFile($izabela_file, 'izabela-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'izabela-'.$tipo_de_acao.'-'.$date);
         }
 
         if(count($granbery_file) > 0)
         {
-            $this->storeFile($granbery_file, 'granbery-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'granbery-'.$subject.'-'.$date);
+            $this->storeFile($granbery_file, 'granbery-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'granbery-'.$tipo_de_acao.'-'.$date);
         }
 
         if(count($fames_file) > 0)
         {
-            $this->storeFile($fames_file, 'fames-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'fames-'.$subject.'-'.$date);
+            $this->storeFile($fames_file, 'fames-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'fames-'.$tipo_de_acao.'-'.$date);
         }
             
         if(count($ipa_file) > 0)
         {
-            $this->storeFile($ipa_file, 'ipa-'.$subject.'-'.$date, $extension, public_path($storage_path));
-            array_push($file_list, 'ipa-'.$subject.'-'.$date);
+            $this->storeFile($ipa_file, 'ipa-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ipa-'.$tipo_de_acao.'-'.$date);
+        }
+
+        return $file_list;
+
+    }
+
+    public function filtroEad($currentFile, $extension, $tipo_de_acao, $date, $storage_path)
+    {
+
+        $tipo_de_acao = str_replace('-a-distancia', '', $tipo_de_acao);
+        $document_hash = md5($tipo_de_acao.$date.date('d-m-y h:i:s'));
+        $headers = [];
+
+        foreach($currentFile[0] as $header => $value)
+            array_push($headers, $header);
+        
+        foreach($currentFile as $key_row => $row)
+        {
+            foreach($row as $key => $cell)
+                if(!($key == 'nome' || $key == 'e_mail' || $key == 'número' || $key == 'ddd'))
+                        unset($currentFile[$key_row][$key]);               
+            
+            $planilha = new Planilha;
+            $planilha->nome = $currentFile[$key_row]['nome'];
+            $planilha->email = $currentFile[$key_row]['e_mail'];
+            $planilha->celular = ($currentFile[$key_row]['número'] && $currentFile[$key_row]['ddd']) ? $currentFile[$key_row]['ddd'].$currentFile[$key_row]['número'] : '';
+            $planilha->instituicao = 'EaD-UMESP';
+            $planilha->documento = $document_hash;
+
+            $planilha->save();
+        }
+
+        Session::put('zipName', "$tipo_de_acao-$date");
+
+        $currentFile = Planilha::select('nome', 'email', 'celular', 'instituicao')
+                                ->where('documento', '=', $document_hash)
+                                ->orderBy('instituicao')
+                                ->orderBy('nome')
+                                ->get()
+                                ->toArray();
+
+        $umesp_file = [];
+        $unimep_file = [];
+        $izabela_file = [];
+        $granbery_file = [];
+        $fames_file = [];
+        $ipa_file = [];
+
+        foreach($currentFile as $row)
+        {
+            switch ($row['instituicao']) {
+                case 'Umesp':
+                    array_push($umesp_file, $row);
+                    break;
+
+                case 'Unimep':
+                    array_push($unimep_file, $row);
+                    break;
+
+                case 'Izabela':
+                    array_push($izabela_file, $row);
+                    break;
+
+                case 'Granbery':
+                    array_push($granbery_file, $row);
+                    break;
+
+                case 'Fames':
+                    array_push($fames_file, $row);
+                    break;
+                    
+                case 'Ipa':
+                    array_push($ipa_file, $row);
+                    break;
+            }
+        }
+
+        $this->clearStorage($storage_path);
+
+        $file_list = [];
+
+        if(count($umesp_file) > 0)
+        {
+            $this->storeFile($umesp_file, 'ead-umesp-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-umesp-'.$tipo_de_acao.'-'.$date);
+        }
+
+        if(count($unimep_file) > 0)
+        {
+            $this->storeFile($unimep_file, 'ead-unimep-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-unimep-'.$tipo_de_acao.'-'.$date);
+        }
+            
+        if(count($izabela_file) > 0)
+        {
+            $this->storeFile($izabela_file, 'ead-izabela-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-izabela-'.$tipo_de_acao.'-'.$date);
+        }
+
+        if(count($granbery_file) > 0)
+        {
+            $this->storeFile($granbery_file, 'ead-granbery-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-granbery-'.$tipo_de_acao.'-'.$date);
+        }
+
+        if(count($fames_file) > 0)
+        {
+            $this->storeFile($fames_file, 'ead-fames-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-fames-'.$tipo_de_acao.'-'.$date);
+        }
+            
+        if(count($ipa_file) > 0)
+        {
+            $this->storeFile($ipa_file, 'ead-ipa-'.$tipo_de_acao.'-'.$date, $extension, public_path($storage_path));
+            array_push($file_list, 'ead-ipa-'.$tipo_de_acao.'-'.$date);
         }
 
         return $file_list;

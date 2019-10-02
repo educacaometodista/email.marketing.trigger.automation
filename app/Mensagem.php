9272f548ba0939129ee78c746dc79648efba1134
Erick <erickluques@gmail.com>
2019-10-01 23:11:23 -0300

    Update Mensagem.php

--- app/Mensagem.php
+++ app/Mensagem.php
@@ -13,19 +13,7 @@ class Mensagem extends Model
         'nome_do_arquivo',
         'conteudo',
         'assunto',
-        'tipo_de_acao_id',
-        'instituicao_id',
     ];
-
-    public function instituicao()
-    {
-        return $this->belongsTo(Instituicao::class, 'instituicao_id');
-    }
-
-    public function tipo_de_acao()
-    {
-        return $this->belongsTo(TipoDeAcao::class, 'tipo_de_acao_id');
-    }
 
     public function getUrl()
     {

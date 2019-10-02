062efbc83a2c8ef8116b32df3edec9c4a43edc45
Erick <erickluques@gmail.com>
2019-10-01 23:11:53 -0300

    Update TipoDeAcao.php

--- app/TipoDeAcao.php
+++ app/TipoDeAcao.php
@@ -12,9 +12,4 @@ class TipoDeAcao extends Model
         'nome',
         'nome_de_exibicao'
     ] ;
-
-    public function mensagens()
-    {
-        return $this->hasMany(Mensagem::class, 'tipo_de_acao_id', 'id');
-    }
 }

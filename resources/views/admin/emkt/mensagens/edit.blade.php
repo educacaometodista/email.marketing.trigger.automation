e0c08910be916f8accdd340e3ab7bdb0f2b44b54
Erick <erickluques@gmail.com>
2019-10-05 06:42:15 -0300

    Update mensagens/edit.blade.php

--- resources/views/admin/emkt/mensagens/edit.blade.php
+++ resources/views/admin/emkt/mensagens/edit.blade.php
@@ -72,44 +72,6 @@
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Tipo de Ação</span>
-                                <div class="col-md-10">
-                                    <select name="tipo_de_acao" class="form-control" id="tipo_de_acao">
-                                        <option></option>
-                                        @foreach($tipos_de_acoes as $tipo_de_acao)
-                                            <option value="{{ $tipo_de_acao->id }}"
-                                            @if($mensagem->tipo_de_acao == $tipo_de_acao->nome)
-                                                selected
-                                            @endif
-                                            >{{ $tipo_de_acao->nome_de_exibicao }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Instituição</span>
-
-                                <div class="col-md-10">
-                                    <select name="instituicao" class="form-control" id="instituicao">
-                                    <option></option>
-                                    @foreach($instituicoes as $instituicao)
-                                        <option value="{{ $instituicao->id }}"
-                                            @if($instituicao->id == $mensagem->instituicao->id)
-                                                selected
-                                            @endif
-                                            
-                                        >{{ $instituicao->nome }}</option>
-                                    @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
                                     <span class="label-text col-md-2 col-form-label text-md-right">Conteúdo</span>
                                 <div class="col-md-10">
                                 <textarea style="height:300px; background-color:#1c2324;color:#eee;font-size:12px;" id="conteudo" name="conteudo" class="form-control" data-trigger="summernote">{{ $mensagem->conteudo }}</textarea>

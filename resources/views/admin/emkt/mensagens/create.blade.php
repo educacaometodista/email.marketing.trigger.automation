4db3eab653adbd3ebb8bb7fff33ea8c056e7ed11
Erick <erickluques@gmail.com>
2019-10-06 15:56:58 -0300

    Update mensagens/create.blade.php

--- resources/views/admin/emkt/mensagens/create.blade.php
+++ resources/views/admin/emkt/mensagens/create.blade.php
@@ -74,35 +74,6 @@
                             </div>
                             <!-- Form Group End -->
 
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Tipo de Ação</span>
-                                <div class="col-md-10">
-                                    <select name="tipo_de_acao" class="form-control" id="tipo_de_acao">
-                                        <option></option>
-                                        @foreach($tipos_de_acoes as $tipo_de_acao)
-                                            <option value="{{ $tipo_de_acao->id }}">{{ $tipo_de_acao->nome_de_exibicao }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                             <!-- Form Group Start -->
-                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Instituição</span>
-
-                                <div class="col-md-10">
-                                    <select name="instituicao" class="form-control" id="instituicao">
-                                    <option></option>
-                                    @foreach($instituicoes as $instituicao)
-                                        <option value="{{ $instituicao->id }}">{{ $instituicao->nome }}</option>
-                                    @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
                         <!-- Form Group Start -->
                         <div class="form-group row">
                                     <span class="label-text col-md-2 col-form-label text-md-right">Conteúdo</span>

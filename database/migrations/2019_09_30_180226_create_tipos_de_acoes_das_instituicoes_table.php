452f0a869acc387dfc095715a44568942b3a40c5
Erick <erickluques@gmail.com>
2019-10-06 18:54:18 -0300

    Campo nome na migration de tipos de ações

--- database/migrations/2019_09_30_180226_create_tipos_de_acoes_das_instituicoes_table.php
+++ database/migrations/2019_09_30_180226_create_tipos_de_acoes_das_instituicoes_table.php
@@ -15,6 +15,7 @@ class CreateTiposDeAcoesDasInstituicoesTable extends Migration
     {
         Schema::create('tipos_de_acoes_das_instituicoes', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nome');
             $table->unsignedInteger('tipo_de_acao_id');
             $table->unsignedInteger('instituicao_id');
             $table->unsignedInteger('filtro_id');

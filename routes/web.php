4db691d691351d5be79d89cbfa3639b51dc4295b
erickfirmo <erickluques@gmail.com>
2019-10-07 18:59:04 -0300

    Rota para upload de arquivos

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::prefix('admin')->name('admin.')->namespace('Emkt')->group(function () {
     Route::prefix('listas')->name('listas.')->group(function () {
         Route::get('create', 'ListaController@create')->name('create');
         Route::get('selecionar-instituicoes', 'ListaController@selecionar_instituicoes')->name('selecionar-instituicoes');
+        Route::post('upload', 'ListaController@upload')->name('update');
         Route::post('store', 'ListaController@store')->name('store');
     });
 

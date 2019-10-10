b6ad87110b8df40757a98ed509fa90dcfaf416fc
Erick <erickluques@gmail.com>
2019-10-10 03:26:28 -0300

    Update TiposDeAcoesDasInstituicoesTableSeeder.php

--- database/seeds/TiposDeAcoesDasInstituicoesTableSeeder.php
+++ database/seeds/TiposDeAcoesDasInstituicoesTableSeeder.php
@@ -11,14 +11,15 @@ class TiposDeAcoesDasInstituicoesTableSeeder extends Seeder
      */
     public function run()
     {
-        //UMESP - Ausentes
+        //UMESP - Ausentes Presencial
         $tipo_de_acao_da_instituicao = new App\TipoDeAcaoDaInstituicao;
-        $tipo_de_acao_da_instituicao->nome = 'Ausentes UMESP Presencial';
+        $tipo_de_acao_da_instituicao->nome = 'UMESP Ausentes Presencial';
         $tipo_de_acao_da_instituicao->tipo_de_acao_id = 1;
         $tipo_de_acao_da_instituicao->instituicao_id = 1;
         $tipo_de_acao_da_instituicao->filtro_id = 1;
         $tipo_de_acao_da_instituicao->mensagem_id = 1;
+        $tipo_de_acao_da_instituicao->nome_da_lista_de_contatos = 'UMESP - Ausentes - [DATE d/m] - [DATE Y]/[DATE HALF]';
+        $tipo_de_acao_da_instituicao->nome_do_arquivo = 'umesp-ausentes-[DATE d-m-Y]-[DATE HALF].csv';
         $tipo_de_acao_da_instituicao->save();
-
     }
 }

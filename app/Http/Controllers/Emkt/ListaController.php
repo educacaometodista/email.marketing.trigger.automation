23802ff82e034f82c71ace6d71c9f50b34593d7f
Erick <erickluques@gmail.com>
2019-10-10 04:58:53 -0300

    Melhorias no método import em ListaController.php

--- app/Http/Controllers/Emkt/ListaController.php
+++ app/Http/Controllers/Emkt/ListaController.php
@@ -109,31 +109,32 @@ class ListaController extends Controller
     {
         $importacao_de_listas = Session::get('importacao-de-listas');
         $tipo_de_acao_id = $importacao_de_listas['tipo_de_acao'];
-        $listas = $importacao_de_listas['arquivos'];
+        $files = $importacao_de_listas['arquivos'];
         $extension = 'csv';
         $date = $importacao_de_listas['data'];
         $instituicoes = Instituicao::whereHas('tipos_de_acoes_da_instituicao', function($query) use($tipo_de_acao_id){
                 $query->where('tipo_de_acao_id', '=', $tipo_de_acao_id);
             }
         )->get();
-        
-        $currentFiles = [];
-        
-        foreach($listas as $lista)
-            array_push($currentFiles, $lista);
 
         $hasAction = false;
-        
+
         $instituicoes_selecionadas = [];
 
         foreach($instituicoes as $instituicao)
         {
+            if($hasAction){
+                if(!is_null($request->input('instituicao-'.strtolower($instituicao->prefixo))))
+                    array_push($instituicoes_selecionadas, $instituicao);
+            } else {
+                array_push($instituicoes_selecionadas, $instituicao);
+            }
         }
 
-        return $this->importSingle($currentFiles, $extension, $instituicoes_selecionadas, $date, $hasAction);
+        return $this->import($files, $extension, $instituicoes_selecionadas, $date, $hasAction);
     }
 
-    public function importSingle($currentFiles, $extension, $instituicoes_selecionadas, $date, $hasAction)
+    public function import($files, $extension, $instituicoes_selecionadas, $date, $hasAction)
     {
         $explode_date = explode('-', str_replace('/', '-', $date));
         $day = $explode_date[0];
@@ -141,17 +142,17 @@ class ListaController extends Controller
         $period = $explode_date[2];
         $period .= $month >=7 ? '-2' : '';
 
-
         if(isset($instituicoes_selecionadas))
         {
-          
-            $listas_de_contatos = $this->planilha()->filter($currentFiles, $extension, $instituicoes_selecionadas, $day.'-'.$month.'-'.$period, 'akna_lists');
-
-            foreach ($listas_de_contatos as $lista_de_contatos)
+            $listas_de_contatos = $this->planilha()->filter($files, $extension, $instituicoes_selecionadas, $day.'-'.$month.'-'.$period, 'akna_lists');
+            foreach ($listas_de_contatos as $prefixo_da_instituicao => $lista_de_contatos)
             {
+                            dd($listas_de_contatos);
+
                 foreach($lista_de_contatos as $contato)
                 {
-
+                    if($instituicao->prefixo == $prefixo_da_instituicao)
+                        $this->aknaAPI()->importarContato($contato, $instituicao);
                 }
             }
 
@@ -171,51 +172,6 @@ class ListaController extends Controller
             }*/
 
             //return $hasAction == true ? $nomes_das_listas : back();
-
-        } else {
-
-            return back()->with('warning', 'Não há instituições cadastradas para importar este arquivo!');
-        }   
-    }
-
-    public function import($currentFile, $extension, $instituicoes, $date, $hasAction)
-    {
-        $explode_date = explode('-', str_replace('/', '-', $date));
-        $day = $explode_date[0];
-        $month = $explode_date[1];
-        $period = $explode_date[2];
-        $period .= $month >=7 ? '-2' : '';
-
-        if(isset($instituicoes))
-        {
-            //campo tipo de acaio em lista create
-            $tipo_de_acao = $instituicoes->first()->tipos_de_acoes_da_instituicao->first()->tipo_de_acao->first()->nome;
-
-            $this->planilha()->filter($currentFile, $extension, $instituicoes, $day.'-'.$month.'-'.$period, 'akna_lists', $this->multiplos_arquivos);
-
-            $all_files = $this->planilha()->getFiles('akna_lists');
-
-            $codigos_dos_processos = [];
-            $nomes_das_listas = [];
-
-            //dd($all_files);
-        
-            foreach($instituicoes as $instituicao)
-            {
-                $nome_do_arquivo = strtolower($this->prefixo[$instituicao->nome]).'-'.str_replace('-a-distancia', '', str_replace(' ', '-', strtolower($tipo_de_acao))).'-'.$day.'-'.$month.'-'.$period.'.'.$extension;
-
-                $nome_do_arquivo = str_replace(' ', '-', $nome_do_arquivo);
-
-                if(in_array(public_path("akna_lists/$nome_do_arquivo"), $all_files))
-                {
-                    $nome_da_lista = 'teste-'.ucwords($this->prefixo[$instituicao->nome]).' - '.str_replace('-', ' ', $tipo_de_acao).' - '.$day.'/'.$month.' - '.str_replace('-', '/',$period);
-                    $status = $this->aknaAPI()->importarListaDeContatos($nome_da_lista, $nome_do_arquivo, $instituicao->nome, $instituicao->codigo_da_empresa);
-                    Session::flash('message-'.$this->prefixo[$instituicao->nome], $status);
-                    $nomes_das_listas[$this->prefixo[$instituicao->nome]] = $nome_da_lista;
-                }
-            }
-
-            return $hasAction == true ? $nomes_das_listas : back();
 
         } else {
 

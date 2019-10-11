ed0680d3ccb97205067a5ecc1abbfeb44c3c1582
Erick <erickluques@gmail.com>
2019-10-11 01:24:21 -0300

    Update TiposDeAcoesDasInstituicoesTableSeeder.php

--- database/seeds/TiposDeAcoesDasInstituicoesTableSeeder.php
+++ database/seeds/TiposDeAcoesDasInstituicoesTableSeeder.php
@@ -11,7 +11,7 @@ class TiposDeAcoesDasInstituicoesTableSeeder extends Seeder
      */
     public function run()
     {
-        //UMESP - Ausentes Presencial
+        //UMESP - Ausentes - Presencial
         $tipo_de_acao_da_instituicao = new App\TipoDeAcaoDaInstituicao;
         $tipo_de_acao_da_instituicao->nome = 'UMESP Ausentes Presencial';
         $tipo_de_acao_da_instituicao->tipo_de_acao_id = 1;
@@ -20,6 +20,26 @@ class TiposDeAcoesDasInstituicoesTableSeeder extends Seeder
         $tipo_de_acao_da_instituicao->mensagem_id = 1;
         $tipo_de_acao_da_instituicao->nome_da_lista_de_contatos = 'UMESP - Ausentes - [DATE d/m] - [DATE Y]/[DATE HALF]';
         $tipo_de_acao_da_instituicao->nome_do_arquivo = 'umesp-ausentes-[DATE d-m-Y]-[DATE HALF].csv';
+        $tipo_de_acao_da_instituicao->save();
+
+        //UMESP - Inscritos Parciais - Presencial
+        $tipo_de_acao_da_instituicao = new App\TipoDeAcaoDaInstituicao;
+        $tipo_de_acao_da_instituicao->nome = 'UMESP Inscritos Parciais Presencial';
+        $tipo_de_acao_da_instituicao->tipo_de_acao_id = 3;
+        $tipo_de_acao_da_instituicao->instituicao_id = 1;
+        $tipo_de_acao_da_instituicao->filtro_id = 1;
+        $tipo_de_acao_da_instituicao->mensagem_id = 2;
+        $tipo_de_acao_da_instituicao->nome_da_lista_de_contatos = 'UMESP - Inscritos Parciais - [DATE d/m] - [DATE Y]/[DATE HALF]';
+        $tipo_de_acao_da_instituicao->nome_do_arquivo = 'umesp-inscritos-parciais-[DATE d-m-Y]-[DATE HALF].csv';
+        $tipo_de_acao_da_instituicao->save();
+
+        //UMESP - Lembrete de Prova - Presencial
+        $tipo_de_acao_da_instituicao = new App\TipoDeAcaoDaInstituicao;
+        $tipo_de_acao_da_instituicao->nome = 'UMESP Lembrete de Prova Presencial';
+        $tipo_de_acao_da_instituicao->tipo_de_acao_id = 5;
+        $tipo_de_acao_da_instituicao->instituicao_id = 1;
+        $tipo_de_acao_da_instituicao->filtro_id = 1;
+        $tipo_de_acao_da_instituicao->mensagem_id = 3;
         $tipo_de_acao_da_instituicao->save();
     }
 }

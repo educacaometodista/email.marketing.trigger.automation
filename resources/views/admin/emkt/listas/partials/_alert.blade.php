ab1e7ec0be5ab8051f0c573c8bd66f87e4a1b085
erickfirmo <erickluques@gmail.com>
2019-10-21 16:56:46 -0300

    Create _alert.blade.php

---
+++ resources/views/admin/emkt/listas/partials/_alert.blade.php
@@ -0,0 +1,25 @@
+@if(isset($instituicoes))
+    @foreach($instituicoes as $instituicao)
+        @if(session('message-success-'.$instituicao->prefixo))
+            <div class="alert alert-success" role="alert">
+                {{ session('message-success-'.$instituicao->prefixo) }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+    @endforeach
+@endif
+
+@if(isset($instituicoes))
+    @foreach($instituicoes as $instituicao)
+        @if(session('message-danger-'.$instituicao->prefixo))
+            <div class="alert alert-danger" role="alert">
+                {{ session('message-danger-'.$instituicao->prefixo) }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+    @endforeach
+@endif

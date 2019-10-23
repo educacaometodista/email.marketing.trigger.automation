08ad027c369a41ed5545266d5abfc91a590254fb
Erick <erickluques@gmail.com>
2019-10-23 01:56:29 -0200

    Update selecionar-instituicoes.blade.php

--- resources/views/admin/emkt/listas/selecionar-instituicoes.blade.php
+++ resources/views/admin/emkt/listas/selecionar-instituicoes.blade.php
@@ -70,7 +70,7 @@
                                                     <option></option>
                                                     <option value="todas">Todas</option>
                                                     @foreach($instituicoes as $instituicao)
-                                                        <option value="{{ $instituicao->tipos_de_acoes_da_instituicao->first()->id }}">{{ $instituicao->nome }}</option>
+                                                        <option value="{{ $instituicao->tipos_de_acoes_da_instituicao->id }}">{{ $instituicao->nome }}</option>
                                                     @endforeach
                                                     </select>
                                                 </div>

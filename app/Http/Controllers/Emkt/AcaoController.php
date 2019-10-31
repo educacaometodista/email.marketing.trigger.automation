59425e3c737afc90bcc5520bfa5b96066f62dfa0
erickfirmo <erickluques@gmail.com>
2019-10-31 19:00:18 -0300

    Update AcaoController.php

--- app/Http/Controllers/Emkt/AcaoController.php
+++ app/Http/Controllers/Emkt/AcaoController.php
@@ -55,34 +55,50 @@ class AcaoController extends Controller
             'date' => 'required|date'
         ]);*/
 
+        $hasList = false;
 
-        if($request->hasFile('import_file'))
-        {
+        if($request->hasFile('import_file') && $request->input('hasList') == 'importar-agora')
+        { 
+            (new ListaController())->saveInSession($request->file('import_file'), date('d-m-Y', strtotime($request->input('date'))), $request->input('tipo_de_acao'));
+            $hasList = 'importar-agora';
 
-            $instituicoes = Instituicao::whereHas('tipos_de_acoes_da_instituicao')->get();
-
-            $data_agendamento = $request->input('data_agendamento');
-            $hora_agendamento = $request->input('hora_agendamento');
-
-            $criacao_de_acao = [];
-            $criacao_de_acao['titulo'] = $request->input('titulo');
-            $criacao_de_acao['data_agendamento'] = $request->input('data_agendamento');
-            $criacao_de_acao['hora_agendamento'] = $request->input('hora_agendamento');
-            $criacao_de_acao['instituicoes_selecionadas_ids'] = [];
-
-            foreach($instituicoes as $instituicao)
-                if(!is_null($request->input('instituicao-'.strtolower($instituicao->prefixo))))
-                    array_push($criacao_de_acao['instituicoes_selecionadas_ids'], $instituicao->id);
-            Session::remove('criacao-de-acao');
-            Session::put('criacao-de-acao', $criacao_de_acao);
-            
-            $importacao_de_listas = (new ListaController())->saveInSession($request->file('import_file'), date('d-m-Y', strtotime($request->input('date'))), $request->input('tipo_de_acao'));
-            
-            return redirect()->route('admin.acoes.selecionar-instituicoes');
-            
+        } else if(!$request->hasFile('import_file') && $request->input('hasList') == 'importar-agora') {
+            //alert "voce deve importar uma lista de contatos"
+            return back();   
         } else {
-            return back();
+            $hasList = 'concluido';
         }
+
+        $instituicoes = Instituicao::whereHas('tipos_de_acoes_da_instituicao')->get();
+        $data_agendamento = $request->input('data_agendamento');
+        $hora_agendamento = $request->input('hora_agendamento');
+        $criacao_de_acao = [];
+        $criacao_de_acao['titulo'] = $request->input('titulo');
+        $criacao_de_acao['data_agendamento'] = $request->input('data_agendamento');
+        $criacao_de_acao['hora_agendamento'] = $request->input('hora_agendamento');
+        $criacao_de_acao['instituicoes_selecionadas_ids'] = [];
+        $criacao_de_acao['hasList'] = $hasList;
+
+
+        foreach($instituicoes as $instituicao)
+            if(!is_null($request->input('instituicao-'.strtolower($instituicao->prefixo))))
+                array_push($criacao_de_acao['instituicoes_selecionadas_ids'], $instituicao->id);
+
+        Session::remove('criacao-de-acao');
+        Session::put('criacao-de-acao', $criacao_de_acao);
+
+        if($hasList == 'concluido')
+        {
+            $this->criar_acao(null);
+
+        } else {
+            return redirect()->route('admin.acoes.selecionar-instituicoes');
+
+        }
+
+        //redirect to - se tiver lista
+        //OU
+        //store - se não tiver
     }
 
     public function selecionar_instituicoes()
@@ -99,8 +115,10 @@ class AcaoController extends Controller
 
             $instituicao_array = [];
 
-            foreach ($instituicoes_selecionadas as $instituicao) {
-                foreach ($instituicao->tipos_de_acoes_da_instituicao as $tipo_de_acao_da_instituicao) {
+            foreach ($instituicoes_selecionadas as $instituicao)
+            {
+                foreach ($instituicao->tipos_de_acoes_da_instituicao as $tipo_de_acao_da_instituicao)
+                {
                     if($tipo_de_acao_da_instituicao->tipo_de_acao_id == $importacao_de_listas['tipo_de_acao'])
                     {
                         $instituicao_array['tipo_de_acao_da_instituicao'] = $tipo_de_acao_da_instituicao->id;
@@ -120,8 +138,51 @@ class AcaoController extends Controller
         }
     }
 
-
     public function store(Request $request)
+    {
+        //acao
+        $criacao_de_acao = Session::get('criacao-de-acao');
+        $hasList = $criacao_de_acao['hasList'];
+
+        //listas
+        $importacao_de_listas = Session::get('importacao-de-listas');
+        $tipo_de_acao_id = $importacao_de_listas['tipo_de_acao'];
+        $files = $importacao_de_listas['arquivos'];
+        $extension = 'csv';
+        $date = $importacao_de_listas['data'];
+        $dados['DATE'] = $date;
+
+        $instituicoes_selecionadas = [];
+
+        foreach ($criacao_de_acao['instituicoes_selecionadas_ids'] as $id_da_instituicao_selecionada)
+            array_push($instituicoes_selecionadas, Instituicao::findOrFail($id_da_instituicao_selecionada));
+
+        
+
+        if($hasList == 'importar-agora')
+        {
+            $i = 0;
+            foreach ($files as $file)
+            {
+                if(!is_null($request->input($file['input_instituicao'])))
+                {
+                    $files[$i]['tipo_de_acao_da_instituicao'] = $request->input($file['input_instituicao']);
+                }
+                $i++;
+            }
+
+            $listas_de_contatos = (new ListaController())->import($files, $extension, $instituicoes_selecionadas, $date, $importacao_de_listas);
+            if(array_key_exists('all', $listas_de_contatos))
+            $listas_de_contatos = $listas_de_contatos['all'];
+
+        } else {
+            $listas_de_contatos = 'importadas';
+        }
+
+        return $this->criar_acao($listas_de_contatos);
+    }
+
+    public function criar_acao($listas_de_contatos)
     { 
         //listas
         $importacao_de_listas = Session::get('importacao-de-listas');
@@ -138,48 +199,36 @@ class AcaoController extends Controller
         $hora_agendamento = $criacao_de_acao['hora_agendamento'];
         $agendamento_envio = $data_agendamento.' '.$hora_agendamento;
 
-        $titulo_da_acao = $titulo.' '.str_replace('-', '/', date('Y-m-d', strtotime($date)));
+        $titulo_da_acao = $titulo.' '.str_replace('-', '/', date('d-m-Y', strtotime($date)));
         
         $instituicoes = Instituicao::whereHas('tipos_de_acoes_da_instituicao', function($query) use($tipo_de_acao_id){
                 $query->where('tipo_de_acao_id', '=', $tipo_de_acao_id);
             }
         )->get();
         
-        //$hasAction = true;
-        $hasAction = false;
         $instituicoes_selecionadas = [];
 
-        //
+        //$instituicao_selecionada = null;
+        /*foreach ($criacao_de_acao['instituicoes_selecionadas_ids'] as $id_da_instituicao_selecionada)
+        {
+            $instituicao_selecionada = Instituicao::findOrFail($id_da_instituicao_selecionada)::with('tipos_de_acoes_da_instituicao')->get();
+                array_push($instituicoes_selecionadas, $instituicao_selecionada);
+        }*/
+
         $instituicoes_selecionadas = $instituicoes;
         
-
-        $i = 0;
-        foreach ($files as $file)
-        {
-            if(!is_null($request->input($file['input_instituicao'])))
-            {
-                $files[$i]['tipo_de_acao_da_instituicao'] = $request->input($file['input_instituicao']);
-            }
-            $i++;
-        }
-
-        $listas_de_contatos = (new ListaController())->import($files, $extension, $instituicoes_selecionadas, $date, $hasAction, $importacao_de_listas);
-
         $response = '';
-
-        if(array_key_exists('all', $listas_de_contatos))
-            $listas_de_contatos = $listas_de_contatos['all'];
 
         foreach ($instituicoes_selecionadas as $instituicao)
         {
-            if(array_key_exists(strtoupper($instituicao->prefixo), $listas_de_contatos))
+            if(array_key_exists(strtoupper($instituicao->prefixo), $listas_de_contatos) || is_null($listas_de_contatos))
             {
-
                 $tipo_de_acao_da_instituicao = TipoDeAcaoDaInstituicao::with('mensagem')
                     ->where('tipo_de_acao_id', $tipo_de_acao_id)
                     ->where('instituicao_id', $instituicao->id)->get()->first();
 
                 $response = (new AknaController())->criarAcaoPontual($titulo_da_acao, $tipo_de_acao_da_instituicao->mensagem, $agendamento_envio, $tipo_de_acao_da_instituicao->instituicao, $tipo_de_acao_da_instituicao->getNomeDaListaDeContatos($dados));
+                //alert
             } 
         }
 

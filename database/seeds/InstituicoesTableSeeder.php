7126fd70ccbc3326f8aa67c0cb987f84eac1680a
erickfirmo <erickluques@gmail.com>
2019-11-22 14:05:22 -0300

    Update InstituicoesTableSeeder.php

--- database/seeds/InstituicoesTableSeeder.php
+++ database/seeds/InstituicoesTableSeeder.php
@@ -28,7 +28,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Metodista de Ensino Superior EaD';
         $instituicao->prefixo = 'EaD-UMESP';
         $instituicao->codigo_da_empresa = $codigos['UMESP'];
-        $instituicao->remetente_do_email = 'Universidade Metodista de São Paulo EaD';
+        $instituicao->remetente_do_email = 'Universidade Metodista de São Paulo';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'informes@metodista.br';
         $instituicao->email_de_retorno = 'informes@metodista.br';
@@ -48,7 +48,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Educacional Piracicabano EaD';
         $instituicao->prefixo = 'EaD-UNIMEP';
         $instituicao->codigo_da_empresa = $codigos['UNIMEP'];
-        $instituicao->remetente_do_email = 'Universidade Metodista de Piracicaba EaD';
+        $instituicao->remetente_do_email = 'Universidade Metodista de Piracicaba';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'unimep@metodista.br';
         $instituicao->email_de_retorno = 'unimep@metodista.br';
@@ -68,7 +68,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Metodista Izabela Hendrix EaD';
         $instituicao->prefixo = 'EaD-Izabela';
         $instituicao->codigo_da_empresa = $codigos['IZABELA'];
-        $instituicao->remetente_do_email = 'Centro Universitário Metodista Izabela Hendrix EaD';
+        $instituicao->remetente_do_email = 'Centro Universitário Metodista Izabela Hendrix';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'informes.imih@izabelahendrix.metodista.br';
         $instituicao->email_de_retorno = 'informes.imih@izabelahendrix.metodista.br';
@@ -88,7 +88,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Metodista Granbery EaD';
         $instituicao->prefixo = 'EaD-Granbery';
         $instituicao->codigo_da_empresa = $codigos['GRANBERY'];
-        $instituicao->remetente_do_email = 'Faculdade Metodista Granbery EaD';
+        $instituicao->remetente_do_email = 'Faculdade Metodista Granbery';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'informes.granbery@granbery.metodista.br';
         $instituicao->email_de_retorno = 'informes.granbery@granbery.metodista.br';
@@ -108,7 +108,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Metodista Centenário EaD';
         $instituicao->prefixo = 'EaD-Fames';
         $instituicao->codigo_da_empresa = $codigos['CENTENARIO'];
-        $instituicao->remetente_do_email = 'Faculdade Metodista Centenário EaD';
+        $instituicao->remetente_do_email = 'Faculdade Metodista Centenário';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'informes.centenario@centenario.metodista.br';
         $instituicao->email_de_retorno = 'informes.centenario@centenario.metodista.br';
@@ -128,7 +128,7 @@ class InstituicoesTableSeeder extends Seeder
         $instituicao->nome = 'Instituto Porto Alegre da Igreja Metodista EaD';
         $instituicao->prefixo = 'EaD-IPA';
         $instituicao->codigo_da_empresa = $codigos['IPA'];
-        $instituicao->remetente_do_email = 'Centro Universitário Metodista IPA EaD';
+        $instituicao->remetente_do_email = 'Centro Universitário Metodista IPA';
         $instituicao->remetente_do_sms = 'EDUCACAO METODISTA';
         $instituicao->email_do_remetente = 'comunicados@ipa.metodista.br';
         $instituicao->email_de_retorno = 'comunicados@ipa.metodista.br';

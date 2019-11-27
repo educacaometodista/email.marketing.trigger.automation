e9b6d5485c4f81c526a4b90fb59356ea0ee9bf28
erickfirmo <erickluques@gmail.com>
2019-11-27 19:50:54 -0300

    Update Acao.php

--- app/Acao.php
+++ app/Acao.php
@@ -11,19 +11,12 @@ class Acao extends Model
     public $fillable = [
         'titulo',
         'destinatarios',
-        'status',
         'agendamento',
         'usuario_id',
-        'tipo_de_acao_da_instituicao_id'
     ];
 
     public function usuario()
     {
         return $this->belongsTo(Admin::class, 'usuario_id');
-    }
-
-    public function tipo_de_acao_da_instituicao()
-    {
-        return $this->belongsTo(TipoDeAcaoDaInstituicao::class, 'tipo_de_acao_da_instituicao_id');
     }
 }

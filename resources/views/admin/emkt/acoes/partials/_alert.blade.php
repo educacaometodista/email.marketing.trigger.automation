b882c9f6d7f4d1916e8cff4da896103af8931312
erickfirmo <erickluques@gmail.com>
2019-11-27 15:29:24 -0300

    Update _alert.blade.php

--- resources/views/admin/emkt/acoes/partials/_alert.blade.php
+++ resources/views/admin/emkt/acoes/partials/_alert.blade.php
@@ -24,3 +24,29 @@
     @endforeach
 @endif
 
+@if(isset($instituicoes))
+    @foreach($instituicoes as $instituicao)
+        @if(session('message-success-sms-'.$instituicao->prefixo))
+            <div class="alert alert-success" role="alert">
+                {{ session('message-success-sms-'.$instituicao->prefixo) }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+    @endforeach
+@endif
+
+@if(isset($instituicoes))
+    @foreach($instituicoes as $instituicao)
+        @if(session('message-danger-sms-'.$instituicao->prefixo))
+            <div class="alert alert-danger" role="alert">
+                {{ session('message-danger-sms-'.$instituicao->prefixo) }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+    @endforeach
+@endif
+

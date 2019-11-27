a95a2b309a15d8429c9bcd8494ffb4749edae78e
erickfirmo <erickluques@gmail.com>
2019-11-27 16:04:54 -0300

    Update AcaoController.php

--- app/Http/Controllers/Emkt/AcaoController.php
+++ app/Http/Controllers/Emkt/AcaoController.php
@@ -251,14 +251,17 @@ class AcaoController extends Controller
 
                 //EMKT
                 $akna_response = (new AknaController())->criarAcaoPontual($titulo_da_acao, $tipo_de_acao_da_instituicao->mensagem, $agendamento_envio, $tipo_de_acao_da_instituicao->instituicao, $tipo_de_acao_da_instituicao->getNomeDaListaDeContatos($dados));
-                
-                //SMS
-                $agendamento_sms = $data_agendamento.'T'.$hora_agendamento.':00';
-                
-                $zenvia_response = (new ZenviaController())->sendMulti($this->setListaDeCelulares($listas_de_contatos[strtoupper($instituicao->prefixo)]), $instituicao->remetente_do_sms, $agendamento_sms, $tipo_de_acao_da_instituicao->mensagem->conteudo_do_sms, $instituicao->nome);
 
                 Session::flash('message-'.$akna_response['status'].'-acao-'.$instituicao->prefixo, $akna_response['message'].' !');
-                Session::flash('message-'.$zenvia_response['status'].'-sms-'.$instituicao->prefixo, $zenvia_response['message'].' !');
+
+                
+                //SMS
+                if($enviar_sms)
+                {
+                    $agendamento_sms = $data_agendamento.'T'.$hora_agendamento.':00';
+                    $zenvia_response = (new ZenviaController())->sendMulti($this->setListaDeCelulares($listas_de_contatos[strtoupper($instituicao->prefixo)]), $instituicao->remetente_do_sms, $agendamento_sms, $tipo_de_acao_da_instituicao->mensagem->conteudo_do_sms, $instituicao->nome);
+                    Session::flash('message-'.$zenvia_response['status'].'-sms-'.$instituicao->prefixo, $zenvia_response['message'].' !');
+                }
 
             }
 

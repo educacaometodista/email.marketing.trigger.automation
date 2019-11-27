3389ef4a0481852c2436ba812456e31a69b033fd
erickfirmo <erickluques@gmail.com>
2019-11-27 19:51:15 -0300

    Update 2019_09_30_192054_create_acoes_table.php

--- database/migrations/2019_09_30_192054_create_acoes_table.php
+++ database/migrations/2019_09_30_192054_create_acoes_table.php
@@ -17,20 +17,13 @@ class CreateAcoesTable extends Migration
             $table->increments('id');
             $table->string('titulo');
             $table->integer('destinatarios');
-            $table->string('status');
             $table->string('agendamento');
-            $table->string('tipo_de_acao');
             $table->unsignedInteger('usuario_id');
-            $table->unsignedInteger('tipo_de_acao_da_instituicao_id');
             $table->timestamps();
 
             $table->foreign('usuario_id')
                 ->references('id')
                 ->on('admins');
-
-            $table->foreign('tipo_de_acao_da_instituicao_id')
-                ->references('id')
-                ->on('tipos_de_acoes_das_instituicoes');
 
         });
     }

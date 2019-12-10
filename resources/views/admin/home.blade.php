195e832451bc008b4e4cc53803d93d3303f306ec
LeuAlmeida <leo@webid.net.br>
2019-12-10 11:09:33 -0300

    Instruções do readme

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -28,24 +28,40 @@
                         </div>
                         <div class="panel-content">
 
-                            <p><h2><i class="fas fa-list-ol"></i> Subindo a Lista Manualmente</h2></p>
-                            <p><h4>1.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>2.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>3.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
+                            <p>
+                                <h2><i class="fas fa-list-ol"></i> Envios da régua de relacionamento</h2>
+                            </p>
+                            <p>
+                                <h4>1. Criando a ação</h4>
+                                No menu lateral <strong>Ações</strong> e clicando em <a href="{{ route('admin.acoes.create') }}"><strong>Criar Nova</strong></a>, você estará no painel de envios da régua.
+                            </p>
 
-                            <hr/>
+                            <p>
+                                <h4>2. Campos a serem preenchidos</h4>
+                                <ul>
+                                    <li><strong>Título:</strong> O nome da ação a ser exibido no Akna;</li>
+                                    <li><strong>Tipo de Ação:</strong> O tipo de ação da régua;</li>
+                                    <li><strong>Instituições:</strong> A instituição presente na régua (em caso de dúvidas, selecionar todas);</li>
+                                    <li><strong>Incluir Data:</strong> Data que estará presente no nome da ação;</li>
+                                    <li><strong>Agendamento:</strong> Data e horário a ser enviado;</li>
+                                    <li>
+                                        <strong>Situação da Lista:</strong> Seleção da base (deve conter os seguintes campos);
+                                        <ul>
+                                            <li>NOME</li>
+                                            <li>DDD <small>(Opcional)</small></li>
+                                            <li>CELULAR</li>
+                                            <li>INSTITUIÇÃO</li>
+                                        </ul>
+                                    </li>
+                                    <li><strong>Arquivo:</strong> Seleção da base para o envio</li>
+                                    <li><strong>Enviar SMS:</strong> Opção para enviar o respectivo SMS daquela ação</li>
+                                </ul>
+                            </p>
 
-                            <p><h2><i class="fas fa-share-alt"></i> Subindo a Ação e Anexando à Lista</h2></p>
-                            <p><h4>1.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>2.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>3.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-
-                            <hr/>
-
-                            <p><h2><i class="fas fa-upload"></i> Realizando o Processo Automatizado</h2></p>
-                            <p><h4>1.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>2.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
-                            <p><h4>3.  Lorem ipsum dolor sit</h4> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi porro vel impedit mollitia ad in voluptate qui ipsum quisquam! Necessitatibus nesciunt aliquam reprehenderit temporibus, est eaque debitis suscipit esse dolorum!</p>
+                            <p>
+                                <h4>3. Modificação em arquivos já existentes</h4>
+                                Todos os arquivos são mantidos e podem ser alterados (incluindo SMS) no campo <a href="{{ route('admin.mensagens.index' ) }}"><strong>mensagens</strong></a>.
+                            </p>
                         </div>
                     </div>
                 </div>
